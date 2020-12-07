de1a51b309e91de59c276584eafb317119aa9797
yelin0323 <48678537+yelin0323@users.noreply.github.com>
2020-12-07 23:31:02 +0900

    Update 3.6.3scipy_linear_regression.py

--- 3.6.3scipy_linear_regression.py
+++ 3.6.3scipy_linear_regression.py
@@ -8,11 +8,9 @@ from scipy import stats
 dow = pdr.get_data_yahoo('^DJI','2000-01-04') #다우존스 지수
 kospi = pdr.get_data_yahoo('^KS11', '2000-01-04')   #코스피 지수
 
-
 df = pd.DataFrame({'X': dow['Close'], 'Y': kospi['Close']}) #다우존스 지수의 종가 칼럼과 코스피 지수의 종가 칼럼으로 데이터 프레임 생성
 df = df.fillna(method='bfill') #데이터 프레임 생성 시 생긴 NaN을 제거
 df = df.fillna(method='ffill')
-
 
 regr = stats.linregress(df.X, df.Y)
 regr_line = f'Y={regr.slope:.2f} * X + {regr.intercept:.2f}'
@@ -28,5 +26,3 @@ plt.title(f'DOW x KOSPI (R={regr.rvalue:.2f})')
 plt.xlabel('Dow Jones Industrial Average')
 plt.ylabel('KOSPI')
 plt.show()
-
-
